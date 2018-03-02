9525c2a857b47f16468ad5547fe2702eae0a6ea0
Tobias Schaber <tobias.schaber@codecentric.de>
2018-03-02 15:53:32 +0100

    running google calendar state

--- aws/lambda/roomChecker/calendarTest.js
+++ aws/lambda/roomChecker/calendarTest.js
@@ -1,14 +1,36 @@
 
 
+
+
 exports.handler = (event, context, callback) => {
 
-    var AWS = require('aws-sdk');
-    AWS.config.update({region: 'eu-central-1'});
+     var AWS = require('aws-sdk');
+     AWS.config.update({region: 'eu-central-1'});
+
+
+    const CONFIG = require('./config/settings');
+    const CalendarAPI = require('node-google-calendar');
+    let cal = new CalendarAPI(CONFIG);
 
     console.log("DA BIN ICH");
 
+    let params = {
+        calendarId: 'tobias.schaber@codecentric.de',
+        timeMin: '2018-03-02T00:00:00-01:00',
+        timeMax: '2018-03-02T18:00:00-01:00',
+    };
 
+    cal.Events.list(params.calendarId, params)
+        .then(resp => {
+            console.log(resp[0].summary);
+        }).catch(err => {
+            console.log(err.message);
+    });
+
+
+    console.log("da war ich");
 };
+
 
 
 
