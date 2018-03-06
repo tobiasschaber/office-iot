d11dcfc228c442bc7a2f5304fd1d8de2d8be5c2e
Tobias Schaber <tobias.schaber@codecentric.de>
2018-03-06 10:04:55 +0100

    replaced private calendar with jakku

--- aws/lambda/roomChecker/calendarTest.js
+++ aws/lambda/roomChecker/calendarTest.js
@@ -2,7 +2,9 @@
 
 
 
-exports.handler = (event, context, callback) => {
+//exports.handler = (event, context, callback) => {
+
+
 
      var AWS = require('aws-sdk');
      AWS.config.update({region: 'eu-central-1'});
@@ -15,24 +17,37 @@ exports.handler = (event, context, callback) => {
     console.log("DA BIN ICH");
 
     let params = {
-        calendarId: 'tobias.schaber@codecentric.de',
+        //calendarId: 'tobias.schaber@codecentric.de',
+        calendarId: 'codecentric.de_3239393533353332373931@resource.calendar.google.com',
         timeMin: '2018-03-02T00:00:00-01:00',
         timeMax: '2018-03-02T18:00:00-01:00',
     };
 
+
+
     cal.Events.list(params.calendarId, params)
         .then(resp => {
-            console.log(resp[0].summary);
+            handleEvents(resp);
         }).catch(err => {
             console.log(err.message);
     });
 
 
     console.log("da war ich");
-};
 
 
 
 
+//};
+
+
+
+function handleEvents(events) {
+    console.log(events);
+    for(i=0; i<events.length; i++) {
+        console.log("event: " + events[i].summary + " " + events[i].status);
+    }
+
+}
 
 
