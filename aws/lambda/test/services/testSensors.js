75c3146cd499a5a0c25a5edaec6c7eac6be79b72
Tobias Schaber <tobias.schaber@codecentric.de>
2018-03-21 09:26:48 +0100

    adjusted test to use new default uuid for room

--- aws/lambda/test/services/testSensors.js
+++ aws/lambda/test/services/testSensors.js
@@ -8,5 +8,5 @@ var callback = function(a)  {
     console.log(a);
 }
 
-sensorsServices.getSensorsForRoom("1", callback);
+sensorsServices.getSensorsForRoom("00000000-0000-0000-0000-000000000000", callback);
 sensorsServices.getRoomForSensor("26e98cf9", callback);
