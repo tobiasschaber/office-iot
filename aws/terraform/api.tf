50ba290648ad0617f74a3164cfcf2182f618a166
Tobias Schaber <tobias.schaber@codecentric.de>
2018-03-21 09:24:12 +0100

    adjusted lambdas for new build location

--- aws/terraform/api.tf
+++ aws/terraform/api.tf
@@ -35,48 +35,48 @@ resource "aws_iam_role_policy_attachment" "attach_lambda_execution_role" {
 # the API lambda to work with rooms
 resource "aws_lambda_function" "create_room_lambda" {
   description = "create a new room"
-  filename = "../lambda/api/build/api.zip"
+  filename = "../lambda/build/lambda.zip"
   function_name = "createRoom"
-  handler = "createRoom.createRoom"
+  handler = "api/createRoom.createRoom"
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "nodejs6.10"
-  source_code_hash = "${base64sha256(file("../lambda/api/build/api.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda/build/lambda.zip"))}"
   timeout = "10"
 }
 
 # the API lambda to attach an existing sensor to a room
 resource "aws_lambda_function" "attach_sensor_to_room_lambda" {
   description = "attach an existing sensor to a room"
-  filename = "../lambda/api/api.zip"
+  filename = "../lambda/build/lambda.zip"
   function_name = "attachSensorToRoom"
-  handler = "attachSensorToRoom.attachSensorToRoom"
+  handler = "api/attachSensorToRoom.attachSensorToRoom"
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "nodejs6.10"
-  source_code_hash = "${base64sha256(file("../lambda/api/api.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda/build/lambda.zip"))}"
   timeout = "10"
 }
 
 # the API lambda to detach a sensor from a room
 resource "aws_lambda_function" "detach_sensor_from_room_lambda" {
   description = "detach a sensor from a room"
-  filename = "../lambda/api/api.zip"
+  filename = "../lambda/build/lambda.zip"
   function_name = "detachSensorFromRoom"
-  handler = "detachSensorFromRoom.detachSensorFromRoom"
+  handler = "api/detachSensorFromRoom.detachSensorFromRoom"
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "nodejs6.10"
-  source_code_hash = "${base64sha256(file("../lambda/api/api.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda/build/lambda.zip"))}"
   timeout = "10"
 }
 
 # the API lambda to list all rooms
 resource "aws_lambda_function" "list_rooms_lambda" {
   description = "list all existing rooms"
-  filename = "../lambda/api/api.zip"
+  filename = "../lambda/build/lambda.zip"
   function_name = "listRooms"
-  handler = "listRooms.listRooms"
+  handler = "api/listRooms.listRooms"
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "nodejs6.10"
-  source_code_hash = "${base64sha256(file("../lambda/api/api.zip"))}"
+  source_code_hash = "${base64sha256(file("../lambda/build/lambda.zip"))}"
   timeout = "10"
 }
 
