139aa3fac7e5a5067d2c35544940b760ad20b3b4
Tobias Schaber <tobias.schaber@codecentric.de>
2018-05-03 11:34:42 +0200

    adjusted motion sensor firmware for better shadow support

--- mongoose/apps/motion-js/fs/init.js
+++ mongoose/apps/motion-js/fs/init.js
@@ -12,7 +12,6 @@ let sensorPin = Cfg.get('app.sensorPin');
 let ledPin = Cfg.get('app.ledPin');
 let frequencyMs = Cfg.get('app.pollFrequency');
 let maxNumberOfSkips = Cfg.get('app.maxNumberOfSkips');
-let buttonPin = 0;
 
 /* last motion state which was sent as update. stored to prevent sending masses of messages */
 let lastDetectedState = 0;
@@ -21,7 +20,7 @@ let lastDetectedState = 0;
 let skippedMessagesCount = 0;
 
 /* state document which is used in the thing shadow */
-let state = { motionDetected: false };
+let state = { motionDetected: 0 };
 
 /* set up pins */
 GPIO.set_mode(sensorPin, GPIO.MODE_INPUT);
@@ -30,21 +29,12 @@ GPIO.set_mode(ledPin, GPIO.MODE_OUTPUT);
 AWS.Shadow.setStateHandler(
   function(data, event, reported, desired, reported_metadata, desired_metadata) {
     if (event === AWS.Shadow.CONNECTED) {
-      AWS.Shadow.update(0, {reported: state});  // Report device state
-    } else if (event === AWS.Shadow.UPDATE_DELTA) {
-      for (let key in state) {
-        if (desired[key] !== undefined) state[key] = desired[key];
-      }
-      AWS.Shadow.update(0, {reported: state});  // Report device state
+        AWS.Shadow.
+      AWS.Shadow.update(0, state);  // Report device state
+
     }
-  print(JSON.stringify(reported), JSON.stringify(desired));
+
 }, null);
-
-
-GPIO.set_button_handler(buttonPin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
-  AWS.Shadow.update(0, {desired: {motionDetected: !state.motionDetected}});
-}, null);
-
 
 
 Timer.set(frequencyMs, true, function() {
@@ -59,7 +49,7 @@ Timer.set(frequencyMs, true, function() {
     } else {
         lastDetectedState = motion;
         skippedMessagesCount = 0;
-        AWS.Shadow.update(0, {desired: {motionDetected: motion}});
+        AWS.Shadow.update(0, {motionDetected: motion});
 
     }
 }, null);
