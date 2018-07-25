f3d5c35d681980fcb778ee4b7b914272f760b3ef
tobias.schaber@codecentric.de <tobias.schaber@codecentric.de>
2018-07-25 13:35:47 +0200

    implemented current occupation status writing logic

---
+++ aws/lambda/src/api/updateCurrentRoomOccupation.js
@@ -0,0 +1,39 @@
+
+const AWS = require('aws-sdk');
+const helper = require('./helper');
+const currentOccupationService = require('../services/currentOccupation');
+const awsRegion = 'eu-central-1';
+
+/**
+ * call this method to set AWS credentials profile,
+ * for example if you want to execute locally
+ */
+exports.setLocalTestMode = (awsCredentialsProfile) => {
+    AWS.config.update({credentials: new AWS.SharedIniFileCredentials({profile: awsCredentialsProfile})});
+    currentOccupationService.setLocalTestMode(awsCredentialsProfile);
+};
+
+
+/**
+ * entry point for lambda execution
+ * @param event
+ * @param context
+ * @param callback
+ */
+exports.updateCurrentRoomOccupation = (event, context, callback) => {
+    AWS.config.update({region: awsRegion});
+
+    if(!event) {
+        console.log("error: event not set");
+    } else {
+        if(!event.sensorId || !event.creationTimestamp) {
+            console.log("error: sensorId or motionDetected or creationTimestamp not set");
+        } else {
+            let sensorId = event.sensorId;
+            let motionDetected = event.motionDetected;
+            let creationTimestamp = event.creationTimestamp;
+
+            currentOccupationService.updateCurrentRoomOccupation(sensorId, motionDetected, creationTimestamp);
+        }
+    }
+};
---
+++ aws/lambda/src/services/currentOccupation.js
@@ -0,0 +1,168 @@
+const AWS = require('aws-sdk');
+const awsRegion = 'eu-central-1';
+const sensorsService = require('./sensors');
+const roomsService = require('./rooms');
+
+
+const currentRoomOccupationTableName = 'currentRoomOccupation';
+
+
+/**
+ * call this method to set AWS credentials profile,
+ * for example if you want to execute locally
+ */
+exports.setLocalTestMode = (awsCredentialsProfile) => {
+    AWS.config.update({credentials: new AWS.SharedIniFileCredentials({profile: awsCredentialsProfile})});
+};
+
+
+exports.updateCurrentRoomOccupation = (sensorId, motionDetected, creationTimestamp) => {
+    AWS.config.update({region: awsRegion});
+    updateCurrentRoomOccupation(sensorId, motionDetected, creationTimestamp);
+}
+
+
+
+/**
+ * function to update the current room occupation table
+ * @param sensorId
+ * @param motionDetected
+ * @param creationTimestamp
+ */
+function updateCurrentRoomOccupation(sensorId, motionDetected, creationTimestamp) {
+
+    var getRoomByIdPromiseWrapper = getRoomBySensorIdPromiseWrapper(sensorId);
+
+    Promise.all([getRoomByIdPromiseWrapper])
+        .then(resp => {
+
+            let roomId = resp[0].attachedInRoom;
+            getCurrentRoomOccupation(roomId, motionDetected, creationTimestamp, updateCurrentRoomOccupationTable);
+
+
+
+        });
+}
+
+
+
+function updateCurrentRoomOccupationTable(roomId, motionDetected, creationTimestamp, currentCreationTimestamp, currentMotionDetected) {
+
+     console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" + roomId + " " + currentCreationTimestamp + " " + currentMotionDetected);
+
+     /* switch from "free" to "occupied" */
+     if(motionDetected === 1) {
+         updateCurrentOccupation(getInsertParamsForUpdateCurrentOccupation(roomId, creationTimestamp, "occupied"));
+
+     } else {
+         if(currentMotionDetected === "occupied" && motionDetected === 0) {
+             if(creationTimestamp >= (currentCreationTimestamp + 10000)) {
+                 updateCurrentOccupation(getInsertParamsForUpdateCurrentOccupation(roomId, creationTimestamp, "free"));
+             }
+         }
+     }
+}
+
+
+
+function getInsertParamsForUpdateCurrentOccupation(roomId, newLastUpdatedTimestamp, newOccupationStatus) {
+
+    var params = {
+        TableName: currentRoomOccupationTableName,
+        Item: {
+            "roomId": roomId,
+            "lastUpdatedTimestamp": newLastUpdatedTimestamp,
+            "occupationStatus": newOccupationStatus
+        }
+    }
+    return params;
+}
+
+
+
+/**
+ * perform an update on the current occupation status table
+ * @param insertParams
+ */
+function updateCurrentOccupation(insertParams) {
+
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    docClient.put(insertParams, function (err, data) {
+        if (err) {
+            console.error("Unable to add notification. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("updated current occupation status table");
+        }
+    });
+
+}
+
+
+
+function getCurrentRoomOccupation(roomId, motionDetected, creationTimestamp, callback) {
+    console.log(roomId);
+    var searchParams = getQueryForGetCurrentRoomOccupation(roomId);
+    var getCurrentRoomOccupationPromiseWrapper = getQueryPromiseWrapper();
+
+    var occupationPromise = getCurrentRoomOccupationPromiseWrapper(searchParams);
+
+    Promise.all([occupationPromise])
+        .then(resp => {
+            let currentCreationTimestamp = resp[0].Items[0].lastUpdatedTimestamp;
+            let currentMotionDetected = resp[0].Items[0].occupationStatus;
+            callback(roomId, motionDetected, creationTimestamp, currentCreationTimestamp, currentMotionDetected);
+        });
+}
+
+
+
+/**
+ * create a promise wrapper for the dynamoDB query, which uses a callback implementation
+ * @param searchparams the parameters for the search
+ * @returns {Promise<any>}
+ */
+function getQueryPromiseWrapper() {
+
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    var wrapper = function (searchParams) {
+        return new Promise((resolve, reject) => {
+            docClient.scan(searchParams, (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(data);
+            });
+        });
+    }
+    return wrapper;
+};
+
+
+
+function getQueryForGetCurrentRoomOccupation(roomId) {
+
+
+    /* motions database query parameters to detect relevant events */
+    var searchparams = {
+        TableName: currentRoomOccupationTableName,
+        ProjectionExpression: "lastUpdatedTimestamp, occupationStatus",
+        FilterExpression: "roomId = :roomId",
+        ExpressionAttributeValues: {
+            ":roomId": roomId
+        }
+    };
+    return searchparams;
+}
+
+
+
+
+function getRoomBySensorIdPromiseWrapper(sensorId) {
+    return new Promise(function(resolve, reject) {
+        sensorsService.getRoomForSensor(sensorId, resolve);
+    });
+}
+
+
